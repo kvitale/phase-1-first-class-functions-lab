fd88e1fa5d69d0f959dfe1f0ad2d9d19d3ce1cbc
Kyle <ktvitale7@gmail.com>
2022-05-04 20:33:05 -0400

    commit tree a5032e38559ec0bc7ab18158f718c066e297bd7b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers){
+    return (arrayOfDrivers.slice(0,2))
+}
+
+// console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+   
+const returnLastTwoDrivers = function(arrayOfDrivers){
+    return (arrayOfDrivers.slice(-2))
+}
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']))
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]   
+
+
+function createFareMultiplier(value){
+    return function(multiplyValue){
+        return value * multiplyValue
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers)
+}
+    
+
+
+    
+
